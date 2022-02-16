cfcbe5fba9ed963998a1f1b05accc25f46a25452
8948036201 <azharuddin.ansari108@gmail.com>
2022-02-16 23:27:53 +0530

    Added: Valid input is accepting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl ="";
   url = "";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -20,5 +21,15 @@ export class AppComponent {
   onClick($event: any) {
     console.log("Save button is clicked", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: { data: any; }){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";
   }
 }
