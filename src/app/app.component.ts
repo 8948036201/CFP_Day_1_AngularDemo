1552a613d1542a07c03e55699466b4f0516c250f
8948036201 <azharuddin.ansari108@gmail.com>
2022-02-16 22:28:30 +0530

    Added: Using Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
-
+  imgUrl ="";
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+    this.imgUrl = "../assets/BridgeLabz-2.jpg";
   }
 }
